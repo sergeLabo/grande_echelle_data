d14135914ad9c783465c958b66bbbf98f5843b97
sergeLabo <sergeblender@labomedia.org>
2022-09-23 10:28:49 +0200

    avec lecture python

--- read_big_data.py
+++ read_big_data.py
@@ -1,8 +1,4 @@
-# grande_echelle_data
 
-data de La Grande Echelle
-
-``` python
 import gzip
 
 fichier = '/media/data/3D/projets/grande_echelle_data/MOBE_1/2022_06_08/cap_2022_06_08_17_20.zip'
@@ -10,4 +6,3 @@ fichier = '/media/data/3D/projets/grande_echelle_data/MOBE_1/2022_06_08/cap_2022
 with gzip.open(fichier, 'rb') as f:
     data = f.read()
 print(data)
-```
